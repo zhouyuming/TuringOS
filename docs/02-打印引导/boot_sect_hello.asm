6b3c7e503774c91a6882d87e72a5ca0054d2ba6a
zhouyuming <1006129869@qq.com>
2024-03-20 23:11:00 +0800

    Create boot_sect_hello.asm

---
+++ docs/02-打印引导/boot_sect_hello.asm
@@ -0,0 +1,23 @@
+mov ah, 0x0e ; tty mode
+mov al, 'T'
+int 0x10
+mov al, 'u'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'i'
+int 0x10 ;
+mov al, 'n'
+int 0x10
+mov al, 'g'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+
+jmp $ ; jump to current address = infinite loop
+
+; padding and magic number
+times 510 - ($-$$) db 0
+dw 0xaa55 
